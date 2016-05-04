0d2627f586bc3468adc85c27ebe206bc836d7e84
Kaarle Ritvanen <kaarle.ritvanen@datakunkku.fi>
2016-05-04 23:37:19 +0300

    rameclock: use ramefbcp font

--- rameclock/clock.c
+++ rameclock/clock.c
@@ -234,7 +234,7 @@ int main(int argc, char **argv)
 	background_rect = create_rect(0, 0, s->screen_width, s->screen_height);
 
 	if (digital) {
-		font = font_load(s, "FreeSerif", 0, analog && ratio > 1 ? s->screen_height / 4 : s->screen_width / 5);
+		font = font_load(s, "ramefbcp", 0, analog && ratio > 1 ? s->screen_height / 4 : s->screen_width / 5);
 		update_time(&tv, &tm, time_buf);
 		font_get_text_extent(font, time_buf, &digital_w, &digital_h);
 	}
